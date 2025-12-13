566c0b3739584011300ed269bed10eafb29dee2d
Edson Batista <edson.batista@conduent.com>
2025-12-13 19:53:02 +0000

    refactor: reorganize JSON instances and schemas into centralized structure

---
+++ packages/factories/src/schemas/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Schemas export for @schepta/factories
+ * 
+ * This module exports JSON schemas that can be used for validation,
+ * autocomplete, and documentation of form and menu configurations.
+ */
+
+export { default as formSchema } from './form-schema.json';
+
+// Future schemas can be added here:
+// export { default as menuSchema } from './menu-schema.json';
+// export { default as stepperSchema } from './stepper-schema.json';
