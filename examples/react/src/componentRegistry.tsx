466296fd454c1e42751da1501208642a2a781c7d
guynikan <36284169+guynikan@users.noreply.github.com>
2025-12-21 13:29:32 +0000

    chore(examples): update React example to use refactored FormFactory
    - Update component registry structure
    - Update form instance with x-ui.order values
    - Simplify App component

---
+++ examples/react/src/componentRegistry.tsx
@@ -0,0 +1,83 @@
+import { createComponentSpec } from "@schepta/core";
+import { FormContainer, FormField, FormSectionContainer, FormSectionTitle, FormSectionGroup, FormSectionGroupContainer } from "./components/FormComponents";
+import { InputText } from "./components/FormComponents";
+import { InputSelect } from "./components/FormComponents";
+import { InputCheckbox } from "./components/FormComponents";
+import { InputTextarea } from "./components/FormComponents";
+import { InputNumber } from "./components/FormComponents";
+import { InputDate } from "./components/FormComponents";
+import { SubmitButton } from "./components/FormComponents";
+
+export const components = {
+  'form-container': createComponentSpec({
+    id: "form-container",
+    type: "form-container",
+    factory: (props, runtime) => FormContainer,
+  }),
+  InputText: createComponentSpec({
+    id: "InputText",
+    type: "field",
+    factory: (props, runtime) => InputText,
+  }),
+  InputSelect: createComponentSpec({
+    id: "InputSelect",
+    type: "field",
+    factory: (props, runtime) => InputSelect,
+  }),
+  InputCheckbox: createComponentSpec({
+    id: "InputCheckbox",
+    type: "field",
+    factory: (props, runtime) => InputCheckbox,
+  }),
+  InputPhone: createComponentSpec({
+    id: "InputPhone",
+    type: "field",
+    factory: (props, runtime) => InputText,
+    defaultProps: { type: "tel" },
+  }),
+  InputTextarea: createComponentSpec({
+    id: "InputTextarea",
+    type: "field",
+    factory: (props, runtime) => InputTextarea,
+  }),
+  InputNumber: createComponentSpec({
+    id: "InputNumber",
+    type: "field",
+    factory: (props, runtime) => InputNumber,
+  }),
+  InputDate: createComponentSpec({
+    id: "InputDate",
+    type: "field",
+    factory: (props, runtime) => InputDate,
+  }),
+  SubmitButton: createComponentSpec({
+    id: "SubmitButton",
+    type: 'content',
+    factory: (props, runtime) => SubmitButton,
+  }),
+  FormField: createComponentSpec({
+    id: "FormField",
+    type: 'container',
+    factory: (props, runtime) => FormField,
+  }),
+  FormSectionContainer: createComponentSpec({
+    id: "FormSectionContainer",
+    type: "container",
+    factory: (props, runtime) => FormSectionContainer,
+  }),
+  FormSectionTitle: createComponentSpec({
+    id: "FormSectionTitle",
+    type: 'content',
+    factory: (props, runtime) => FormSectionTitle,
+  }),
+  FormSectionGroupContainer: createComponentSpec({
+    id: "FormSectionGroupContainer",
+    type: 'container',
+    factory: (props, runtime) => FormSectionGroupContainer,
+  }),
+  FormSectionGroup: createComponentSpec({
+    id: "FormSectionGroup",
+    type: 'container',
+    factory: (props, runtime) => FormSectionGroup,
+  }),
+};
